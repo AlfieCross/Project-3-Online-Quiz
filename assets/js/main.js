1e891fdbabf2041d4e58d1bab1e11c169dd85724
Alfie Cross <alfiecross@unknown.home>
2025-07-27 09:09:19 +0100

    split js.script into seperate js files

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// =============================
+// main.js — Entry point script
+// =============================
+
+// Load questions globally
+import { questions } from "./questions.js";
+
+// Wait until the DOM is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.body;
+
+  if (body.classList.contains("quiz-page")) {
+    initQuiz(questions);
+  }
+
+  if (body.classList.contains("results-page")) {
+    showResults();
+  }
+});
